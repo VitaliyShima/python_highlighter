53ee9f45e3ba47e35210aaa4b39982d9f690e71c
Vitaliy <vitaliyshima@gmail.com>
2019-02-03 02:45:47 +0200

    complit init file

--- highlighter/__init__.py
+++ highlighter/__init__.py
@@ -31,9 +31,9 @@ def create_app():
         """Markup given text.
         @:param text - string text to be marked
         @:return marked text, e.g., <mark>highlighted text</mark>."""
-        result = text
 
-        # TODO: add an implementation
+        text = "<mark>" + text + "</mark>"
+        result = text
 
         return result
 
@@ -41,9 +41,10 @@ def create_app():
         """Markup searched string in given text.
         @:param text - string text to be processed
         @:return marked text, e.g., "sample text <mark>highlighted part</mark> rest of the text"."""
-        result = text
 
-        # TODO: add an implementation
+        if expr in text:
+            text = text.replace(expr, markup_text(expr))
+        result = text
 
         return result
 
